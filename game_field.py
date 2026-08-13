ff014b1e8ec3db8195634e56002e21a61f60b3a9
jbt <shirelgrama18@gmail.com>
2026-08-13 16:20:51 +0300

    commit 5

---
+++ game_field.py
@@ -0,0 +1,17 @@
+import pygame
+
+def ENTER():
+    # פה זה צריך ללכת למסך השנילמשך כמה שניות
+    pass
+
+def DOWN():
+    pass
+
+def UP():
+    pass
+
+def LEFT():
+    pass
+
+def RIGHT():
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+import pygame
+
+import game_field
+import screen
+
+def main():
+    # להוסיף את פונקציה של המסך הירוק
+    while 1>0:
+        for event in pygame.event.get():
+            if event.type == pygame.K_KP_ENTER:
+                game_field.ENTER()
+            elif event.type == pygame.KEYDOWN:
+                game_field.DOWN()
+            elif event.type == pygame.KEYUP:
+                game_field.UP()
+            elif event.type == pygame.K_LEFT:
+                game_field.LEFT()
+            elif event.type == pygame.K_RIGHT:
+                game_field.RIGHT()
+        # if החייל נוגע במוקש - אז להתפוצץ- נפסל
+        # אם החייל פוגע בדגל- אז ניצח
+        # לשנות את המסך לפי התזוזה של החייל
