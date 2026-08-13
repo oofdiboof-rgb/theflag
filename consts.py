651512483fb87fddad8947fbe4bc842afc0a437e
jbt <shirelgrama18@gmail.com>
2026-08-13 14:01:07 +0300

    commit 3

--- consts.py
+++ consts.py
@@ -1,26 +1,31 @@
 import pygame
 
+SCREEN_X=1000
+SCREEN_Y=500
+SCREEN_COLOR=(9,99,0)
+
+MATRICS_X=50
+MATRICS_Y=25
+
 GRASS_IMG=pygame.image.load('grass.png')
-GRASS_SIZE=(90,90)
+GRASS_SIZE=(SCREEN_X/MATRICS_X*3  ,SCREEN_Y/MATRICS_Y*3)
 GRASS=pygame.transform.scale(GRASS_IMG,GRASS_SIZE)
 
 MINE_IMG=pygame.image.load('mine.png')
-MINE_SIZE=(90,30)
+MINE_SIZE=(SCREEN_X/MATRICS_X*3,SCREEN_Y/MATRICS_Y)
 MINE=pygame.transform.scale(MINE_IMG,MINE_SIZE)
 
 FLAG_IMG=pygame.image.load('flag.png')
-FLAG_SIZE=(90,120)
+FLAG_SIZE=(SCREEN_X/MATRICS_X*3,SCREEN_Y/MATRICS_Y*4)
 FLAG=pygame.transform.scale(FLAG_IMG,FLAG_SIZE)
-FLAG_START=(1410, 630)
+FLAG_START=(SCREEN_X-FLAG_SIZE[0], SCREEN_Y-FLAG_SIZE[1])
 
-EXPLOTION=pygame.image.load('explotion.png')
+EXPLOSION=pygame.image.load('explotion.png')
 INJURY=pygame.image.load('injury.png')
-INJURY_SIZE=(120,60)
+INJURY_SIZE=(SCREEN_X/MATRICS_X*4,SCREEN_Y/MATRICS_Y*2)
 
 SOLDIER=pygame.image.load('soldier.png')
-SOLDIER_SIZE=(120,60)
+SOLDIER_SIZE=INJURY_SIZE
 START_SOLDIER=(0, 0)
 
-SCREEN_X=1500
-SCREEN_Y=750
-SCREEN_COLOR=(9,99,0)
+
