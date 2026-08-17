30d70647eca46d34b0e6080df1a811afc129d98a
jbt <shirelgrama18@gmail.com>
2026-08-17 16:59:02 +0300

    commit 12

--- soldier.py
+++ soldier.py
@@ -1,7 +1,7 @@
-# from screenfile import screen
+from screenfile import screen_create
 import pygame
 import random
 import consts
-from consts import  FLAG_START, NORMAL_COLOR, XRAY_COLOR, SOLDIER
-def create_soldier(screen):
+from consts import FLAG_START, NORMAL_COLOR, XRAY_COLOR
+def create_soldier():
     screen.blit(consts.SOLDIER, (consts.X_SOLDIER, consts.Y_SOLDIER))
