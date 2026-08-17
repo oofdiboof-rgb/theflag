9219c014523c8e846117af0cb58c3ccecff70338
oofdiboof <oofdiboof@gmail.com>
2026-08-17 14:41:03 +0300

    commit grass screen working

--- game_field.py
+++ game_field.py
@@ -1,27 +1,37 @@
 import pygame
-
-import main
 import screenfile
 import consts
 import random
 
-from consts import XRAY_COLOR, MATRICS_Y, MATRICS_X
 
 
 def create_matrix():
     for row in range(consts.MATRICS_Y):
         consts.MATRIX.append([])
         for col in range(consts.MATRICS_X):
-            consts.MATRIX[row].append('0')
+            consts.MATRIX[row].append(0)
+    for i in range(3):
+        for j in range(4):
+            consts.MATRIX[i+22][j+46] = "DEGEL"
 
-def spawn_mines():
+def spawn_mines(screen):
+    create_matrix()
     for mines in range(20):
-        row = random.choice(range(len(consts.MATRIX)))
-        spot = random.choice(range(len(consts.MATRIX[row])))
-        if (consts.MATRIX[row][spot] or consts.MATRIX[row][spot+1] or consts.MATRIX[row][spot+2]) != "MINE":
-            consts.MATRIX[row][spot] = "MINE"
-            consts.MATRIX[row][spot+1] = "MINE"
-            consts.MATRIX[row][spot+2] = "MINE"
+        BOOL = True
+        while BOOL:
+            row = random.choice(range(consts.MATRICS_Y))
+            spot = random.choice(range(len(consts.MATRIX[row]) - 2))
+            if (consts.MATRIX[row][spot] or consts.MATRIX[row][spot + 1] or consts.MATRIX[row][spot + 2]) == 0:
+                BOOL = False
+        screen.blit(consts.MINE, ((consts.GRID_POS_X*spot, consts.GRID_POS_Y*row)))
+        consts.MATRIX[row][spot] = "MINE"
+        consts.MATRIX[row][spot+1] = "MINE"
+        consts.MATRIX[row][spot+2] = "MINE"
+        BOOL = True
+
+    for i in consts.MATRIX:
+        print(i)
+
 
 
 # screen.blit(consts.FLAG, FLAG_START)
@@ -42,3 +52,16 @@ def spawn_mines():
 #     clock.tick(60)
 # screenfile.screen_create()
 # banans()
+# spawn_mines()
+# for i in consts.MATRIX:
+#     print(i)
+
+#
+# draw_screen-
+#
+# draw_grid-
+#
+# if visible:
+#     draw_screen --
+# else:
+#     draw
--- screenfile.py
+++ screenfile.py
@@ -1,27 +1,32 @@
 import pygame
 import random
 import consts
-import soldier
-from consts import  FLAG_START, NORMAL_COLOR, XRAY_COLOR
+import game_field
+from consts import START_SOLDIER, FLAG_START, NORMAL_COLOR, XRAY_COLOR
 
 
 def screen_create():
     pygame.init()
+    spawn_grass()
     global screen
     screen = pygame.display.set_mode((consts.SCREEN_X, consts.SCREEN_Y))
     clock = pygame.time.Clock()
     screen.fill(NORMAL_COLOR)
-    spawn_grass()
-    soldier.create_soldier(screen)
+    for i in grass_places:
+        screen.blit(consts.GRASS, i)
+    # game_field.spawn_mines(screen)
     # screen.blit(consts.SOLDIER, START_SOLDIER)
     # screen.blit(consts.SOLDIER, START_SOLDIER)
     pygame.display.flip()
-    # clock.tick(60)
+    clock.tick(60)
 
 
 def spawn_grass():
+    global grass_places
+    grass_places = []
     for i in range(20):
-        screen.blit(consts.GRASS, ((random.choice(range(int(consts.SCREEN_X-consts.GRASS_SIZE[0])))), random.choice(range(int(consts.SCREEN_Y-consts.GRASS_SIZE[1])))))
-def spawn_mine():
-    for i in range(20):
-        screen.blit(consts.MINE, ((random.choice(range(int(consts.SCREEN_X-consts.MINE_SIZE[0])))), random.choice(range(int(consts.SCREEN_Y-consts.MINE_SIZE[1])))))
+        grass_places.append((random.choice(range(int(consts.SCREEN_X-consts.GRASS_SIZE[0]))), (random.choice(range(int(consts.SCREEN_Y-consts.GRASS_SIZE[1]))))))
+    return grass_places
+# def spawn_mine():
+#     for i in range(20):
+#         screen.blit(consts.MINE, ((random.choice(range(int(consts.SCREEN_X-consts.MINE_SIZE[0])))), random.choice(range(int(consts.SCREEN_Y-consts.MINE_SIZE[1])))))
