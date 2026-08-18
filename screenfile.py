1f6ff00a0bf26e371d86cf176678903bda299347
oofdiboof <oofdiboof@gmail.com>
2026-08-18 14:22:19 +0300

    commit day 3

--- screenfile.py
+++ screenfile.py
@@ -9,11 +9,11 @@ def screen_create():
     global screen
     clock = pygame.time.Clock()
     screen.fill(NORMAL_COLOR)
+    for coords in grass_places:
+        screen.blit(consts.GRASS, coords)
     Font = pygame.font.SysFont('comic sans', 18)
     font= Font.render('Welcome to The Flag game.\n Have Fun!', False, 'orange')
     screen.blit(font, (consts.GRID_POS_X*2, 0))
-    for coords in grass_places:
-        screen.blit(consts.GRASS, coords)
     screen.blit(FLAG, FLAG_START)
     create_soldier()
     # game_field.spawn_mines(screen)
