c9d9af3fcd56ce93a0e0ef0c6abc84eb814d8a98
oofdiboof <oofdiboof@gmail.com>
2026-08-18 11:30:08 +0300

    commit day 3

--- screenfile.py
+++ screenfile.py
@@ -9,9 +9,9 @@ def screen_create():
     global screen
     clock = pygame.time.Clock()
     screen.fill(NORMAL_COLOR)
-    create_soldier()
     for coords in grass_places:
         screen.blit(consts.GRASS, coords)
+    create_soldier()
     # game_field.spawn_mines(screen)
     # screen.blit(consts.SOLDIER, START_SOLDIER)
     # screen.blit(consts.SOLDIER, START_SOLDIER)
